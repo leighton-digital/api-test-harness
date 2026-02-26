275d164274afad535f79151c154e4d790f61cae2
Ronnie R. Winter <ronnie.winter@leighton.com>
2026-02-26 17:05:22 +0000

    fix: fix issues with jest and uuid

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
 
 const config: Config = {
   silent: true,
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['/node_modules/(?!(\\.pnpm|uuid))'],
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts'],
@@ -24,7 +27,21 @@ const config: Config = {
           },
         },
         module: {
-          type: 'commonjs',
+          type: 'es6',
+        },
+      },
+    ],
+    '^.+\\.js$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'ecmascript',
+          },
+          target: 'es2021',
+        },
+        module: {
+          type: 'es6',
         },
       },
     ],
